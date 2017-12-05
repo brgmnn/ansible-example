612b625a17c8294d66bbaf08a4c2eb45c46ab205
Daniel Bergmann <brgmnn@users.noreply.github.com>
2017-12-05 10:29:40 -0800

    Add basic node server playbook

---
+++ app-hello-world/server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var fs = require('fs');
+var app = express()
+
+var html = fs.readFileSync('./hello-world.html');
+
+app.get('/', function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(html);
+})
+
+app.listen(3000, function () {
+  console.log('Node app listening on port 3000! Running node', process.version)
+})
