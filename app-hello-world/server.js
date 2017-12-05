b60a413f99129bf7f103612e311b50074805ea3f
Daniel Bergmann <brgmnn@users.noreply.github.com>
2017-12-05 14:12:43 -0800

    Finish node install scripts

--- app-hello-world/server.js
+++ app-hello-world/server.js
@@ -2,11 +2,11 @@ var express = require('express')
 var fs = require('fs');
 var app = express()
 
-var html = fs.readFileSync('./hello-world.html');
+var html = fs.readFileSync('./hello-world.html')
 
 app.get('/', function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end(html);
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.end(html)
 })
 
 app.listen(3000, function () {
